ad25436025dd6314e61321a7766cf0199358b0fb
Kimberly Encinas <kimbencinas@gmail.com>
2024-01-12 18:18:24 -0800

    set up tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,17 @@ import './App.css';
 export default function App() {
 
     return (
-        <div className='app'>
-            <Sidebar />
-            <MainContent />
-            <Task />
-            <Calendar />
+        <div className="flex">
+            <div className="w-1/5">
+                <Sidebar />
+            </div>
+            <div className="w-3/5">
+                <MainContent />
+                <Task />
+            </div>
+            <div className="w-1/5">
+                <Calendar />
+            </div>
         </div>
     );
 }
--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -15,9 +15,11 @@ export default function MainContent() {
     }, []);
 
     return (
-        <div className='main-content'>
-            <h1 className='current-day-title'>Today's Tasks</h1>
-            <h3 className='todays-date'>{currentDate}</h3>
+        <div className="app flex">
+            <div className="main-content bg-gray-300 pt-4">
+                <h1 className='current-day-title'>Today's Tasks</h1>
+                <h3 className='todays-date'>{currentDate}</h3>
+            </div>
         </div>
     );
 }
