368da9119dc7326076480cca23876c272ea83caa
Kimberly Encinas <kimbencinas@gmail.com>
2024-01-18 00:06:04 -0800

    fix sizing for the task daily tasks section

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function Task({ time, taskDesc }) {
 
     return (
-        <div className="task bg-white p-7 w-full ml-7 mb-3 mt-5 rounded-xl">
-            <p className="text-gray-500 mr-5 mb-5">{time}</p>
+        <div className="task bg-white p-7 w-81 ml-7 mb-3 mt-5 mr-7 rounded-xl">
+            <p className="text-gray-500 mr-5 mb-3">{time}</p>
             <div className="flex items-center mb-2">
                 <input
                     type="radio"
