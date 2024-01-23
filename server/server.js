27138bda85e519d0a8c952d8af4f27f21de94ac0
Kimberly Encinas <kimbencinas@gmail.com>
2024-01-22 23:40:22 -0800

    getting started w authentication

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const app = express()
-const jwt = require('jsonwebtoken')
+const express = require('express');
+const app = express();
+const jwt = require('jsonwebtoken');
+const mysql = require('mysql12');
 
 app.use(express.json())
 
+//Dummy data
 app.get("/api", (req, res) => {
-    res.json({ "tasks": ["taskOne", "taskTwo", "taskThree"] })
-})
+    res.json({ "tasks": ["taskOne", "taskTwo", "taskThree"] });
+});
 
-app.listen(5000, () => { console.log("Server started on port 5000") })
+app.post("/api/login", (req, res) => {
+    const username = req.body.username;
+    const user = { name: username };
+});
+
+app.post('/api/tasks', (req, res) => {
+    res.json({
+
+    });
+});
+
+app.listen(5000, () => { console.log("Server started on port 5000") });
