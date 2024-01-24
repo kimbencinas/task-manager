3a9d49130571a6e7ad25528296279eba7c5df9d1
Kimberly Encinas <kimbencinas@gmail.com>
2024-01-24 14:44:35 -0800

    connect to mysql database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const jwt = require('jsonwebtoken');
-const mysql = require('mysql12');
+const mysql = require('mysql2');
 
 app.use(express.json())
+
+const db = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Successfully connected to database.');
+    }
+});
 
 //Dummy data
 app.get("/api", (req, res) => {
