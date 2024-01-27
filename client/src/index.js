0a638ca8b1274a705ec93aa82f98467dc8e4e828
Kimberly Encinas <kimbencinas@gmail.com>
2024-01-26 16:11:18 -0800

    add more to react router

--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,31 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import App from "./App.jsx";
+import ScheduledEvents from './Components/ScheduledEvents.jsx';
+import Task from './Components/Task.jsx';
+import Calendar from './Pages/Calendar.jsx';
+import CreateTask from './Pages/CreateTask.jsx';
+import MainContent from './Pages/MainContent.jsx';
+import Sidebar from './Pages/Sidebar.jsx';
+import Login from './Pages/Login.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-  }
+  },
+  {
+    path: '/create-task',
+    element: <CreateTask />
+  },
+  {
+    path: '/scheduled-events',
+    element: <ScheduledEvents />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
