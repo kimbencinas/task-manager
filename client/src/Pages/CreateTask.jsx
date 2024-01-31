78d12bc627add7e6516385c3d244e618f458c78b
Kimberly Encinas <kimbencinas@gmail.com>
2024-01-30 23:11:15 -0800

    changes to create task page design

--- client/src/Pages/CreateTask.jsx
+++ client/src/Pages/CreateTask.jsx
@@ -19,33 +19,37 @@ export default function CreateTask() {
     }
 
     return (
-        <div className="main-container">
-            <h2>Create a task</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="task_time" className="text-black">
-                        Task time:
-                    </label>
-                    <input
-                        type="text"
-                        id="task_time"
-                        value={taskDetails.task_time}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div>
-                    <label htmlFor="task_description" className="text-black">
-                        Task description
-                    </label>
-                    <input
-                        type="text"
-                        id="task_description"
-                        value={taskDetails.task_description}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <button type="submit" className="bg-blue-500 text-white rounded-lg p-2 text-sm h-9">Submit</button>
-            </form>
+        <div className="main-container flex items-center justify-center h-screen">
+            <div>
+                <h2 className="font-medium mb-4">Create a task</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-4">
+                        <label htmlFor="task_time" className="text-black">
+                            Task time:
+                        </label>
+                        <input
+                            type="text"
+                            id="task_time"
+                            value={taskDetails.task_time}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="task_description" className="text-black">
+                            Task description
+                        </label>
+                        <input
+                            type="text"
+                            id="task_description"
+                            value={taskDetails.task_description}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div className="flex justify-end">
+                        <button type="submit" className="bg-blue-500 text-white rounded-lg p-2 text-sm h-9">Submit</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
