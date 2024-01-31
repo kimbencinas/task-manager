9191cf2ba7c094982355c24fe46cf9baf41b787c
Kimberly Encinas <kimbencinas@gmail.com>
2024-01-31 14:53:05 -0800

    design finished for create task page

--- client/src/Pages/CreateTask.jsx
+++ client/src/Pages/CreateTask.jsx
@@ -20,11 +20,11 @@ export default function CreateTask() {
 
     return (
         <div className="main-container flex items-center justify-center h-screen">
-            <div>
-                <h2 className="font-medium mb-4">Create a task</h2>
+            <div className="bg-white bg-opacity-50 rounded-xl p-24 w-6/12">
+                <h2 className="font-medium mb-8 text-4xl text-center">Create a task</h2>
                 <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label htmlFor="task_time" className="text-black">
+                    <div className="mb-4 p-2">
+                        <label htmlFor="task_time" className="text-black block text-lg mb-3 font-medium">
                             Task time:
                         </label>
                         <input
@@ -32,10 +32,11 @@ export default function CreateTask() {
                             id="task_time"
                             value={taskDetails.task_time}
                             onChange={handleInputChange}
+                            className="w-full rounded-md drop-shadow-lg h-8"
                         />
                     </div>
-                    <div className="mb-4">
-                        <label htmlFor="task_description" className="text-black">
+                    <div className="mb-11 p-2">
+                        <label htmlFor="task_description" className="text-black block text-lg mb-3 font-medium">
                             Task description
                         </label>
                         <input
@@ -43,10 +44,11 @@ export default function CreateTask() {
                             id="task_description"
                             value={taskDetails.task_description}
                             onChange={handleInputChange}
+                            className="w-full rounded-md drop-shadow-lg h-8"
                         />
                     </div>
                     <div className="flex justify-end">
-                        <button type="submit" className="bg-blue-500 text-white rounded-lg p-2 text-sm h-9">Submit</button>
+                        <button type="submit" className="bg-blue-500 text-white rounded-xl text-md h-9 w-24 drop-shadow-lg">Submit</button>
                     </div>
                 </form>
             </div>
