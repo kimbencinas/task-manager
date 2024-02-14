4b8f143e9efc12655a922a5259e07a49226a7359
Kimberly Encinas <kimbencinas@gmail.com>
2024-02-13 23:54:08 -0800

    fixed up logo for sidebar

--- client/src/Pages/Sidebar.jsx
+++ client/src/Pages/Sidebar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './../img/taskzen.png';
+import taskzen from '../img/taskzen-logo.png';
 
 export default function Sidebar() {
 
     return (
         <div className="sidebar mt-2">
             <h2>TaskZen</h2>
+            <img src={taskzen} />
         </div>
     );
 }
