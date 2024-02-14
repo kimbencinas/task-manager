f66cc325f1db7fd69ba405fbcaa4c0d93a94ad3a
Kimberly Encinas <kimbencinas@gmail.com>
2024-02-13 23:50:22 -0800

    remove unnecessary files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,8 +25,6 @@ export default function App() {
             </div>
             <div className="w-3/4 bg-white bg-opacity-50 ml-4 rounded-xl">
                 <MainContent />
-                <Task time="7:30am" taskDesc="Start presentation slides" />
-                <Task time="8:45am" taskDesc="Meeting with team" />
             </div>
             <div className="w-1/4 bg-white bg-opacity-50 ml-4 mr-5 rounded-xl">
                 <Calendar />
--- client/src/Pages/Sidebar.jsx
+++ client/src/Pages/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './../img/taskzen.png';
 
 export default function Sidebar() {
 
