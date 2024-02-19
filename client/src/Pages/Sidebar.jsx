bf5c8ec5f244aa3cd6a893d883ebdad25eb4ae34
Kimberly Encinas <kimbencinas@gmail.com>
2024-02-18 16:34:20 -0800

    logo now directs user to homepage

--- client/src/Pages/Sidebar.jsx
+++ client/src/Pages/Sidebar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import taskzen from '../img/taskzen-logo.png';
 
 export default function Sidebar() {
 
     return (
         <div className="sidebar mt-2">
-            <img src={taskzen} className="mt-4 scale-90" />
+            <Link to="/">
+                <img src={taskzen} className="mt-4 scale-90 mt-6" />
+            </Link>
         </div>
     );
 }
