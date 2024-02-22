d1f70838ba5eb95fb47ee02d9fbc643c371b214e
Kimberly Encinas <kimbencinas@gmail.com>
2024-02-21 22:07:22 -0800

    sidebar changes

--- client/src/Pages/Sidebar.jsx
+++ client/src/Pages/Sidebar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import taskzen from '../img/taskzen-logo.png';
+import usericon from '../img/user-icon.png'
+import settingsicon from '../img/settings-icon.png'
 
 export default function Sidebar() {
 
     return (
         <div className="sidebar mt-2">
-            <h2>TaskZen</h2>
             <img src={taskzen} />
+            <img src={usericon} />
+            <img src={settingsicon} />
         </div>
     );
 }
