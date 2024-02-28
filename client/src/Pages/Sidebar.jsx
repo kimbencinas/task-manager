c5c897403c86e96d55443c1697019183979ad42a
Kimberly Encinas <kimbencinas@gmail.com>
2024-02-28 12:25:18 -0800

    added directory for tasks page & create task page

--- client/src/Pages/Sidebar.jsx
+++ client/src/Pages/Sidebar.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 import taskzen from '../img/taskzen-logo.png';
 import usericon from '../img/user-icon.png';
 import settingsicon from '../img/settings-icon.png';
-import tasklogo from '../img/tasklogo.png';
+import createtask from '../img/tasklogo.png';
+import taskicon from '../img/tasks-page-icon.png';
 import { Link } from 'react-router-dom';
 
 export default function Sidebar() {
 
     return (
-        <div className="sidebar mt-2">
+        <div className="sidebar mt-2 flex flex-col items-center gap-2">
             <Link to="/">
                 <img src={taskzen} className="w-13" />
             </Link>
-            <img src={usericon} className="sm:w-24 md:w-32 lg:w-40 xl:w-48" />
-            <img src={settingsicon} />
+            <img src={usericon} className="size-14" />
+            <img src={settingsicon} className="size-14" />
+            <Link to="/create-task">
+                <img src={createtask} className="size-14" />
+            </Link>
             <Link to="/events">
-                <img src={tasklogo} />
+                <img src={taskicon} className="w-36" />
             </Link>
         </div>
     );
