f583590d1e0daa317564242bea992e35fb077639
Kimberly Encinas <kimbencinas@gmail.com>
2024-02-27 21:39:03 -0800

    user now able to access events page from the sidebar

--- client/src/Pages/Sidebar.jsx
+++ client/src/Pages/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import taskzen from '../img/taskzen-logo.png';
 import usericon from '../img/user-icon.png';
 import settingsicon from '../img/settings-icon.png';
+import tasklogo from '../img/tasklogo.png';
 import { Link } from 'react-router-dom';
 
 export default function Sidebar() {
@@ -13,6 +14,9 @@ export default function Sidebar() {
             </Link>
             <img src={usericon} className="sm:w-24 md:w-32 lg:w-40 xl:w-48" />
             <img src={settingsicon} />
+            <Link to="/events">
+                <img src={tasklogo} />
+            </Link>
         </div>
     );
 }
