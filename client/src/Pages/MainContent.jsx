47241fbff88788468681419ea74ad2b8503ff6da
Kimberly Encinas <kimbencinas@gmail.com>
2024-02-29 21:46:46 -0800

    add button for removing tasks

--- client/src/Pages/MainContent.jsx
+++ client/src/Pages/MainContent.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import CreateTask from './CreateTask';
 import Task from '../Components/Task';
+import trashicon from '../img/trash-icon.png';
 
 export default function MainContent() {
     const [currentDate, setCurrentDate] = useState('');
@@ -56,6 +57,7 @@ export default function MainContent() {
                 <Link to="/create-task" className="ml-auto">
                     <button className="bg-black text-white rounded-lg p-2 text-sm h-9 ml-auto">Create task</button>
                 </Link>
+                <img src={trashicon} className="size-3" />
             </div>
             <h3 className="todays-date ml-8 mb-2 text-lg">{currentDate}</h3>
             {tasks.map((task, index) => (
--- server/server.js
+++ server/server.js
@@ -83,4 +83,10 @@ app.get('/api/tasks', (req, res) => {
     })
 })
 
+/*
+app.delete('/api/tasks', (req, res) => {
+
+})
+*/
+
 app.listen(5000, () => { console.log("Server started on port 5000") });
