253f1600771ecddffe6a97b38d8ee3e6e09b5fed
Kimberly Encinas <kimbencinas@gmail.com>
2024-03-03 12:29:35 -0800

    minor design changes to the layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,13 +20,13 @@ export default function App() {
 
     return (
         <div className="app flex items-stretch justify-center h-screen p-9">
-            <div className="w-1/12 bg-white bg-opacity-50 ml-5 rounded-xl">
+            <div className="w-1/12 bg-white bg-opacity-50 rounded-xl">
                 <Sidebar />
             </div>
             <div className="w-3/4 bg-white bg-opacity-50 ml-4 rounded-xl">
                 <MainContent />
             </div>
-            <div className="w-1/4 bg-white bg-opacity-50 ml-4 mr-5 rounded-xl">
+            <div className="w-1/4 bg-white bg-opacity-50 ml-4 rounded-xl">
                 <Calendar />
             </div>
         </div>
