6c769f79a27dd414e07b8669913a59991b322e3a
Kimberly Encinas <kimbencinas@gmail.com>
2024-04-15 23:42:01 -0700

    frontend finished for login page

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -3,8 +3,41 @@ import React from 'react';
 export default function Login() {
 
     return (
-        <div>
-            <h2>Log In</h2>
+        <div className="main-container flex items-center justify-center h-screen">
+            <div className="bg-white bg-opacity-50 rounded-xl p-24 w-6/12">
+                <h2 className="font-medium mb-8 text-4xl text-center">Log In</h2>
+                <form>
+                    <div className="mb-4 p-2">
+                        <label htmlFor="user_username" className="text-black block text-lg mb-3 font-medium">
+                            Username:
+                        </label>
+                        <input
+                            type="text"
+                            id="user_name"
+                            name="user_name"
+                            className="w-full rounded-md drop-shadow-lg p-2"
+                        />
+                    </div>
+                    <div className="mb-4 p-2">
+                        <label htmlFor="user_password" className="text-black block text-lg mb-3 font-medium">
+                            Password:
+                        </label>
+                        <input
+                            type="text"
+                            id="user_password"
+                            name="user_password"
+                            className="w-full rounded-md drop-shadow-lg p-2"
+                        />
+                    </div>
+                    <div className="flex justify-end">
+                        <button
+                            type="submit"
+                            className="bg-blue-500 text-white rounded-xl text-md h-9 w-24 drop-shadow-lg">
+                            Submit
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
